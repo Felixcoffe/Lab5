b584428ae87c7c08f2c50ef2e94c64d07a625376
Felixcoffe <Felipe.valenzuelahu@usm.cl>
2022-10-17 15:28:34 -0300

    commit 2
    fix

--- INFJMC/INFJMC/urls.py
+++ INFJMC/INFJMC/urls.py
@@ -1,4 +1,4 @@
-"""misitio URL Configuration
+"""INFJMC URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
