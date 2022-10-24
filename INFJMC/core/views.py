18cf18e0a7c4acafcf9f34165a0a8521022722e9
Felixcoffe <Felipe.valenzuelahu@usm.cl>
2022-10-24 14:59:43 -0300

    Update views.py

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+0
+menu = "<hr><ul>"
+menu +=   " <li><a href=''>Inicio</a>"
+menu +=   " <li><a href='carreras/'>Carreras</a>"
+menu +=   " <li><a href='docentes/'>Docentes</a>"
+menu += "</ul>"
 
-# Create your views here.
 def home(request):
     return render(request,'core/home.html')
 
 def carreras(request):
-    titulo = "<h><>"
-    return HttpResponse("carreras")
+    titulo = "<h1>Carreras</h1>"
+    contenido = "<hr><p>Lorem ipsum dolor sit amet."
+    html = titulo + menu + contenido
+    return HttpResponse(html)
 
 def docentes(request):
+    titulo = "<h1>Docentes</h1>"
     return HttpResponse("docentes")
